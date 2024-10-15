186007ee5e96b823c1fbce569caef754cda387c6
Tobias Recker <Recker@match.uni-hannover.de>
2024-10-15 21:54:23 +0200

    added maze world fixed ground plane

---
+++ mir_robot/mir_gazebo/launch/mir_robot_spawn.launch.py
@@ -0,0 +1,203 @@
+import os
+import xacro
+from pathlib import Path
+from ament_index_python.packages import get_package_share_directory
+
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument, ExecuteProcess, IncludeLaunchDescription
+from launch.actions import RegisterEventHandler, SetEnvironmentVariable
+from launch.event_handlers import OnProcessExit
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
+from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
+
+
+def generate_launch_description():
+    # Launch Arguments
+    use_sim_time = LaunchConfiguration('use_sim_time', default=True)
+
+    fws_robot_description_path = os.path.join(
+        get_package_share_directory('fws_robot_description'))
+    
+    mir_description_path = os.path.join(
+        get_package_share_directory('mir_description'))
+    
+    fws_robot_sim_path = os.path.join(
+        get_package_share_directory('mir_gazebo'))
+
+    # Set gazebo sim resource path
+    gazebo_resource_path = SetEnvironmentVariable(
+        name='GZ_SIM_RESOURCE_PATH',
+        value=[
+            os.path.join(fws_robot_sim_path, 'worlds'), ':' +
+            str(Path(fws_robot_description_path).parent.resolve())
+            ]
+        )
+
+    arguments = LaunchDescription([
+                DeclareLaunchArgument('world', default_value='maze',
+                          description='Gz sim World'),
+           ]
+    )
+
+    gazebo = IncludeLaunchDescription(
+                PythonLaunchDescriptionSource([os.path.join(
+                    get_package_share_directory('ros_gz_sim'), 'launch'), '/gz_sim.launch.py']),
+                launch_arguments=[
+                    ('gz_args', [LaunchConfiguration('world'),
+                                 '.world',
+                                 ' -v 4',
+                                 ' -r']
+                    )
+                ]
+             )
+
+    xacro_file = os.path.join(mir_description_path, 'urdf', 'mir_600', 'mir_600.gazebo.urdf')
+
+    doc = xacro.process_file(xacro_file, mappings={'use_sim' : 'true'})
+
+    robot_desc = doc.toprettyxml(indent='  ')
+    robot_controllers = PathJoinSubstitution(
+        [
+            FindPackageShare('mir_description'),
+            'config',
+            'diffdrive_controller.yaml',
+        ]
+    )
+
+    params = {'robot_description': robot_desc}
+    
+    node_robot_state_publisher = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        output='screen',
+        parameters=[params]
+    )
+
+    gz_spawn_entity = Node(
+        package='ros_gz_sim',
+        executable='create',
+        output='screen',
+        arguments=['-string', robot_desc,
+                   '-x', '0.0',
+                   '-y', '0.0',
+                   '-z', '0.07',
+                   '-R', '0.0',
+                   '-P', '0.0',
+                   '-Y', '0.0',
+                   '-name', 'fws_robot',
+                   '-allow_renaming', 'false'],
+    )
+    control_node = Node(
+        package="controller_manager",
+        executable="ros2_control_node",
+        parameters=[robot_controllers],
+        output="both",
+        remappings=[
+            ("/diffbot_base_controller/cmd_vel", "/cmd_vel"),
+        ],
+    )
+
+    gpio_controller_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["mobile_base_controller", 
+                   "-c", "/controller_manager",
+                   "-t", "diff_drive_controller/DiffDriveController", robot_controllers
+                  ],
+)
+
+    joint_state_broadcaster_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["joint_state_broadcaster"],
+    )
+
+    load_joint_state_controller = ExecuteProcess(
+        cmd=['ros2', 'control', 'load_controller', '--set-state', 'active',
+             'joint_state_broadcaster'],
+        output='screen'
+    )
+
+    load_forward_velocity_controller = ExecuteProcess(
+        cmd=['ros2', 'control', 'load_controller', '--set-state', 'active',
+             'mobile_base_controller'],
+        output='screen'
+    )
+
+    # launch rqt_robot_steering
+    rqt_robot_steering = Node(
+        package='rqt_robot_steering',
+        executable='rqt_robot_steering',
+        output='screen'
+    )
+
+    # launch repub_twist.py
+    repub_twist = Node(
+        package='fws_robot_sim',
+        executable='repub_twist.py',
+        output='screen'
+    )
+
+    # Bridge
+    bridge = Node(
+        package='ros_gz_bridge',
+        executable='parameter_bridge',
+        arguments=['/b_scan@sensor_msgs/msg/LaserScan@gz.msgs.LaserScan','/f_scan@sensor_msgs/msg/LaserScan@gz.msgs.LaserScan'],
+        output='screen'
+    )
+
+    rviz_config_file = os.path.join(fws_robot_description_path, 'config', 'fws_robot_config.rviz')
+
+    rviz = Node(
+        package="rviz2",
+        executable="rviz2",
+        name="rviz2",
+        output="log",
+        arguments=["-d", rviz_config_file],
+    )
+
+    # Delay start of joint_state_broadcaster after `robot_controller`
+    # TODO(anyone): This is a workaround for flaky tests. Remove when fixed.
+    # delay_joint_state_broadcaster_after_robot_controller_spawner = RegisterEventHandler(
+    #     event_handler=OnProcessExit(
+    #         target_action=robot_controller_spawner,
+    #         on_exit=[joint_state_broadcaster_spawner],
+    #     )
+    # )
+
+    # nodes = [
+    #     control_node,
+    #     robot_controller_spawner,
+    #     delay_joint_state_broadcaster_after_robot_controller_spawner,
+    # ]
+
+    return LaunchDescription([
+        RegisterEventHandler(
+            event_handler=OnProcessExit(
+                target_action=gz_spawn_entity,
+                on_exit=[load_joint_state_controller],
+            )
+        ),
+        RegisterEventHandler(
+            event_handler=OnProcessExit(
+               target_action=load_joint_state_controller,
+               on_exit=[load_forward_velocity_controller],
+            )
+        ),
+        gazebo_resource_path,
+        arguments,
+        gazebo,
+        node_robot_state_publisher,
+        gz_spawn_entity,
+        bridge,
+        rviz,
+        #control_node,
+        # gpio_controller_spawner,
+        # joint_state_broadcaster_spawner,
+        # load_joint_state_controller,
+        # load_forward_velocity_controller,
+        rqt_robot_steering,
+        repub_twist
+    ])
