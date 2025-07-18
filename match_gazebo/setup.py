f7b65d0c33b032bcc83261d24f864fee9c637f9a
pumablattlaus <heeren@match.uni-hannover.de>
2025-07-18 14:23:44 +0200

    fix: Refactor setup.py to use dynamic world file discovery (directories can't be copied directly)

--- match_gazebo/setup.py
+++ match_gazebo/setup.py
@@ -4,16 +4,19 @@ import os
 
 package_name = 'match_gazebo'
 
+world_files = [f for f in glob('worlds/**/*', recursive=True) if os.path.isfile(f)]               # exclude directories
+
 setup(
     name=package_name,
     version='0.0.0',
     packages=find_packages(exclude=['test']),
+
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
         (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py')),
-        ('share/' + package_name + '/worlds',  glob('worlds/*')),
+        ('share/' + package_name + '/worlds',  world_files),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
